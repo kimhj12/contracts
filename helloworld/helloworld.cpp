b3125b1a0b2c9af12469146efc6c5550e6c1e509
kimhj12 <sdi30@naver.com>
2019-07-17 13:41:17 +0900

    contract

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,18 @@
+#include <eosiolib/eosio.hpp>
+
+using namespace eosio;
+
+CONTRACT helloworld: public contract{
+    public:
+        using contract::contract;
+
+        ACTION hi(name user) {
+            check(has_auth(user), "missing authority");
+            // == require_auth(user)
+
+            //has_auth() -> true or false
+
+            print("hello, ", user);
+        }
+    private:
+};
