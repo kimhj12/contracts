b5677a3002aa49705c28d2170af009eb3ea0fa87
kimhj12 <sdi30@naver.com>
2019-07-18 12:35:26 +0900

    contract 2

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -7,12 +7,26 @@ CONTRACT helloworld: public contract{
         using contract::contract;
 
         ACTION hi(name user) {
-            check(has_auth(user), "missing authority");
+            require_auth(user);
+            print("hello, ", user);    
+         }
+            //check(has_auth(get_self()) || has_auth("moonmoon1234"_n), "you aint a contract deployer!!");
+            
+            // bool isTrue = has_auth(get_self()) || has_auth("hahahoho1234"_n);
+            // check(isTrue, "you aint a contract deployer!!")
+
             // == require_auth(user)
 
             //has_auth() -> true or false
 
-            print("hello, ", user);
+            //require_auth(get_self);
+            //check(has_auth(get_self(), "missing authority"));
+
+         ACTION getaccount(name user){
+            require_auth(get_self());
+            print(is_account(user));
         }
+
+        
     private:
 };
